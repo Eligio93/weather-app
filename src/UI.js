7a8bf4be2d40d4be99efe0371e8e39e569d6396a
Eligio93 <eligio.cristantielli@gmail.com>
2023-11-11 16:41:18 +1100

    display top div with current date and time

--- src/UI.js
+++ src/UI.js
@@ -1,3 +1,7 @@
+import { getCurrentData } from "./Api-functions";
+import { createInfoObject } from "./App";
+
+
 let displayHome= function(){
     let form=document.createElement("form");
     form.id="form";
@@ -13,5 +17,33 @@ let displayHome= function(){
     document.body.appendChild(form);
 }
 
-export {displayHome};
+let displayInfo= async function(tempUnit){
+        try{
+            let infoObject=await createInfoObject();
+            if(infoObject!=="No matching found"){
+                let topDiv=document.createElement("div");
+                topDiv.id="top-div";
+                let date=document.createElement("p");
+                date.id="date";
+                date.textContent=infoObject.date;
+                topDiv.appendChild(date);
+                let time=document.createElement("p");
+                time.id="time";
+                time.textContent=infoObject.time;
+                topDiv.appendChild(time);
+                document.body.appendChild(topDiv)
+            }
+        }catch(error){
+            console.log("Errore: "+ error);
+        }
+
+
+}
+    //preleva la city nell'input
+
+
+
+
+
+export {displayHome,displayInfo};
 
