921c31503826c81cc3118ac1b7b72c8779c97335
Eligio93 <eligio.cristantielli@gmail.com>
2023-11-14 13:55:44 +1100

    init app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("ciao");
+import { displayInfo, displayHome } from "./UI";
+
+displayHome();
+let searchBtn = document.getElementById("search-btn");
+searchBtn.addEventListener("click",function (event) {
+    event.preventDefault();
+    let city = document.getElementById("search-bar").value;
+    let defaultUnit = "C";
+    displayInfo(city,defaultUnit)
+});
+
+
