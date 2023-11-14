2eece612f3e6492dc1031187ea2f17ba987b298d
Eligio93 <eligio.cristantielli@gmail.com>
2023-11-14 18:18:30 +1100

    fix bug http

--- src/Api-functions.js
+++ src/Api-functions.js
@@ -1,7 +1,7 @@
 let getCurrentData= async function(city){
     try{
         let api="45e67828ff3b497ebb033714230111";
-        let response= await fetch('http://api.weatherapi.com/v1/current.json?key='+api+'&q='+city+'&aqi=no',{mode:'cors'});
+        let response= await fetch('https://api.weatherapi.com/v1/current.json?key='+api+'&q='+city+'&aqi=no',{mode:'cors'});
         let currentData= await response.json();
         return currentData        
     }catch(error){
@@ -11,7 +11,7 @@ let getCurrentData= async function(city){
 }
 let getForecastData=async function(city){
     let api="45e67828ff3b497ebb033714230111";
-    let response=await fetch('http://api.weatherapi.com/v1/forecast.json?key='+api+'&q='+city+'&days=7&aqi=no&alerts=no',{mode:'cors'});
+    let response=await fetch('https://api.weatherapi.com/v1/forecast.json?key='+api+'&q='+city+'&days=7&aqi=no&alerts=no',{mode:'cors'});
     let forecastData=await response.json();
     return forecastData
 
